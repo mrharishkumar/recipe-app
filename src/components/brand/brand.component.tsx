f202c232f66f58032633038bc97dea7e1b9fa290
harish.kumar <harish.kumar@impressico.com>
2023-06-29 16:49:54 +0530

    Add styles from props

--- src/components/brand/brand.component.tsx
+++ src/components/brand/brand.component.tsx
@@ -1,7 +1,24 @@
 import logo from "./logo.png";
 
-const Brand = () => {
-  return <img src={logo} alt="aramark-logo" />;
+export interface BrandProps {
+  align?: any;
+  width?: string | number;
+  padding?: number;
+}
+
+const Brand = ({ align, width, padding }: BrandProps) => {
+  const style = {
+    float: align ? align : "left",
+    paddingTop: padding ? `${padding / 2}px` : "20px",
+    paddingBottom: padding ? `${padding / 2}px` : "20px",
+    paddingLeft: padding ? `${padding}px` : "40px",
+    paddingRight: padding ? `${padding}px` : "40px",
+  };
+  return (
+    <div style={{ width: width || "100vw" }}>
+      <img style={style} src={logo} alt="aramark-logo" />
+    </div>
+  );
 };
 
 export default Brand;
