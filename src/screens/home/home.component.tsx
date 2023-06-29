2db0e72b4af2cec169fcba6a6008e6a7650de46d
harish.kumar <harish.kumar@impressico.com>
2023-06-29 16:50:16 +0530

    Add grid

--- src/screens/home/home.component.tsx
+++ src/screens/home/home.component.tsx
@@ -1,20 +1,25 @@
+import { Grid } from "@mui/material";
 import Brand from "../../components/brand/brand.component";
 import SimpleList from "../../components/simpleList/simpleList.component";
 
 const Home = () => {
   return (
-    <>
-      <Brand />
-      <SimpleList
-        tabs={[
-          { label: "Tags", component: <>Hello</> },
-          { label: "Tags", component: <>Hello</> },
-          { label: "Tags", component: <>Hello</> },
-          { label: "Tags", component: <>Hello</> },
-          { label: "Tags", component: <>Hello</> },
-        ]}
-      />
-    </>
+    <Grid container>
+      <Grid item>
+        <Brand />
+      </Grid>
+      <Grid item>
+        <SimpleList
+          tabs={[
+            { label: "Tags", component: <>Hello</> },
+            { label: "Tags", component: <>Hello</> },
+            { label: "Tags", component: <>Hello</> },
+            { label: "Tags", component: <>Hello</> },
+            { label: "Tags", component: <>Hello</> },
+          ]}
+        />
+      </Grid>
+    </Grid>
   );
 };
 
