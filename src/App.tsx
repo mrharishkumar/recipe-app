ef7d9dde32b85e71bec532c969f33ede16c6914a
harish.kumar <harish.kumar@impressico.com>
2023-06-29 16:23:52 +0530

    Add home componenet to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import "./App.css";
+import Home from "./screens/home/home.component";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Home />
+    </div>
+  );
 }
 
 export default App;
