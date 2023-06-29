36617ed5d4b5e4ea92a020598f0fef1cebc9e1de
harish.kumar <harish.kumar@impressico.com>
2023-06-29 18:50:42 +0530

    Add example data

--- src/screens/home/home.component.tsx
+++ src/screens/home/home.component.tsx
@@ -1,7 +1,16 @@
 import { Grid } from "@mui/material";
 import Brand from "../../components/brand/brand.component";
 import SimpleList from "../../components/simpleList/simpleList.component";
-import { RecipeList } from "../../components/recipeList/recipeList";
+import {
+  RecipeList,
+  IRecipeList,
+} from "../../components/recipeList/recipeList";
+
+const recipeList: IRecipeList[] = [
+  { name: "recipe", recipe: "recipe" },
+  { name: "recipe1", recipe: "recipe1" },
+  { name: "recipe2", recipe: "recipe2" },
+];
 
 const Home = () => {
   return (
@@ -12,11 +21,18 @@ const Home = () => {
       <Grid item>
         <SimpleList
           tabs={[
-            { label: "Tags", component: <RecipeList /> },
-            { label: "Tags", component: <RecipeList /> },
-            { label: "Tags", component: <RecipeList /> },
-            { label: "Tags", component: <RecipeList /> },
-            { label: "Tags", component: <RecipeList /> },
+            {
+              label: "Tags",
+              component: <RecipeList recipeList={recipeList} />,
+            },
+            {
+              label: "Recomendations",
+              component: <RecipeList recipeList={recipeList} />,
+            },
+            {
+              label: "Settings",
+              component: <RecipeList recipeList={recipeList} />,
+            },
           ]}
         />
       </Grid>
